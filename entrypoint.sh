7d44d06fd03f6651a66154d04dcfb9fe94d913cd
Garik <khvatik.igor@gmail.com>
2025-01-15 15:44:32 +0300

    add loglevel to gunicorn entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,8 @@ gunicorn app.main:app \
 -w "${WORKERS_COUNT:-4}" \
 -k uvicorn.workers.UvicornWorker \
 --threads "${THREADS_COUNT:-8}" \
---bind 0.0.0.0:80
+--bind 0.0.0.0:80 \
+--log-level "${LOG_LEVEL:-info}"
 
 #uvicorn app.main:app --host 0.0.0.0 --port 80 --reload
 exec "$@"
